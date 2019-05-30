76d5e14176e1c457e9667e24e7ce9f7148faf3a3
Steve <stevetasticsteve@gmail.com>
2019-05-30 19:34:45 +1000

    Added a rotating file handler for logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import flask, flask_sqlalchemy, flask_migrate, flask_login
+import logging, os
 from config import Config
+from logging.handlers import RotatingFileHandler
 
 app = flask.Flask(__name__)
 app.config.from_object(Config)
@@ -7,5 +9,18 @@ db = flask_sqlalchemy.SQLAlchemy(app)
 migrate = flask_migrate.Migrate(app, db)
 login = flask_login.LoginManager(app)
 login.login_view = 'login'
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 from app import routes, models, errors
